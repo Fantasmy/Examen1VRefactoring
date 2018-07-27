42f80183d6a526839f8602ddba82fa6740f3864b
Jenny <fantazmit@gmail.com>
2018-07-28 00:39:13 +0200

    Added Automapper configuration. Couldn't add in Common Layer because of the circular reference. Close #13.

---
+++ Vueling.Infrastructure.Repository/Repository/AutomapperConfig.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vueling.Domain.Entities;
+using Vueling.Infrastructure.Repository.DataModel;
+
+namespace Vueling.Infrastructure.Repository.Repository
+{
+    class AutomapperConfig
+    {
+        public static MapperConfiguration writeConfig;
+
+        public static MapperConfiguration readConfig;
+        static AutomapperConfig()
+        {
+            writeConfig = new MapperConfiguration(cfg => cfg.CreateMap<ClientEntity, Clients>()); 
+
+            readConfig = new MapperConfiguration(cfg => cfg.CreateMap<ClientEntity, Clients>()
+            .ForMember(dest => dest.id, sou => sou.UseValue("Private")));
+        }
+    }
+}
